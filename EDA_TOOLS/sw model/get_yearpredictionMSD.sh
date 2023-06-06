ec357d414a206d434cf4df29f48b7c6f0128c269
sxw228 <43896882+sxw228@users.noreply.github.com>
2023-06-06 11:46:52 +0800

    Add files via upload

---
+++ EDA_TOOLS/sw model/get_yearpredictionMSD.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script will grab the LibSVM formatted version of the
+# YearPredictionMSD data set if we don't already have it
+
+if [ ! -d "YearPredictionMSD" ]; then
+	mkdir YearPredictionMSD
+fi
+
+cd ./YearPredictionMSD
+if [ -f "YearPredictionMSD" ]; then
+	echo "[YearPredictionMSD] Nothing to do. Data is there."
+fi
+if [ ! -f "YearPredictionMSD" ]; then
+	wget --no-check-certificate https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.bz2
+	bzip2 -d YearPredictionMSD.bz2
+fi
+if [ -f "YearPredictionMSD.t" ]; then
+	echo "[YearPredictionMSD.t] Nothing to do. Data is there."
+fi
+if [ ! -f "YearPredictionMSD.t" ]; then
+	wget --no-check-certificate https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.t.bz2
+	bzip2 -d YearPredictionMSD.t.bz2
+fi
+
